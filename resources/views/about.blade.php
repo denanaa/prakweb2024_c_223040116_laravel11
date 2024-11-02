7a0aed965e6500ebf783f95d3133c09741147542
napollux <denanaw30@gmail.com>
2024-11-02 17:53:58 +0700

    Video #3 Struktur Folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+    <h1>Halaman About Me</h1>
+    <h3>Owner : {{ $nama }} <h3>
+    <img src="img/profile.jpg" alt="Winter Aespa" width="200">
+</body>
+
+</html>
