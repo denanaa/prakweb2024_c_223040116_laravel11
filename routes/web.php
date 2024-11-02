da66478a500272f94147b25021a6034ba8a80bb0
napollux <denanaw30@gmail.com>
2024-11-03 00:29:02 +0700

    Video #7 Model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -12,45 +13,13 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Dena Astia',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut aliquam nisi debitis accusamus distinctio sed reiciendis ullam magnam corrupti quo ab qui repellendus consequatur cupiditate eius magni, aspernatur deserunt porro?',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Dena Awg',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto id exercitationem labore reprehenderit libero hic itaque nihil voluptatem repudiandae? Voluptatibus dolorum, cumque omnis sequi repellat molestiae ab saepe amet culpa!',
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()
+    ]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Dena Astia',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut aliquam nisi debitis accusamus distinctio sed reiciendis ullam magnam corrupti quo ab qui repellendus consequatur cupiditate eius magni, aspernatur deserunt porro?',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Dena Awg',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto id exercitationem labore reprehenderit libero hic itaque nihil voluptatem repudiandae? Voluptatibus dolorum, cumque omnis sequi repellat molestiae ab saepe amet culpa!',
-        ]
-    ];
 
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post= Post::find($slug);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
