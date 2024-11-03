1c79975ca2ca20652b3785d68d7473fa1a901c08
napollux <denanaw30@gmail.com>
2024-11-03 07:52:48 +0700

    Video #9 Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,37 +1,12 @@
-<?php 
+<?php
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Dena Astia',
-                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut aliquam nisi debitis accusamus distinctio sed reiciendis ullam magnam corrupti quo ab qui repellendus consequatur cupiditate eius magni, aspernatur deserunt porro?',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Dena Awg',
-                'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto id exercitationem labore reprehenderit libero hic itaque nihil voluptatem repudiandae? Voluptatibus dolorum, cumque omnis sequi repellat molestiae ab saepe amet culpa!',
-            ]
-        ]; 
-    }
-
-    public static function find($slug): array {
-        $post = Arr::first(static::all(),fn($post) => $post['slug'] == $slug);
-
-        if(! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
